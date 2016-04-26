05ccfc8238557a1cf5e02ce08b86b3daf4cf0325
mizuky fujitani <mizuky fujitani>
2016-04-26 21:17:54 +0900

    バージョン1.1.1　CSVダウンロードでHTTPステータス406が返される問題への対策

--- src/main/java/org/unclazz/metaversion/controller/CsvController.java
+++ src/main/java/org/unclazz/metaversion/controller/CsvController.java
@@ -3,13 +3,15 @@ package org.unclazz.metaversion.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.unclazz.metaversion.service.CommitService;
 import org.unclazz.metaversion.service.ProjectParallelsService;
 
@@ -21,22 +23,26 @@ public class CsvController {
 	private CommitService commitService;
 	
 	@RequestMapping(value = "/csv/projects/{projectId}/parallels",
-			consumes = MediaType.ALL_VALUE,
 			produces = MediaType.APPLICATION_OCTET_STREAM_VALUE,
 			method = RequestMethod.GET)
- 	@ResponseBody
- 	public Resource getProjectsParallelsListCsvDownload(
+ 	public ResponseEntity<byte[]> getProjectsParallelsListCsvDownload(
  			@PathVariable("projectId") final int id) throws IOException {
- 		return parallelsService.getProjectParallelsCsvByProjectId(id);
+ 		final byte[] bytes = parallelsService.getProjectParallelsCsvByProjectId(id);
+ 		final HttpHeaders headers = new HttpHeaders();
+ 		headers.add("contet-type", MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE + ";Shift_JIS");
+ 		headers.set("Content-Disposition", String.format("filename=\"parallels_%s.csv\"", id));
+ 		return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/csv/projects/{projectId}/changedpaths",
-			consumes = MediaType.ALL_VALUE,
 			produces = MediaType.APPLICATION_OCTET_STREAM_VALUE,
 			method = RequestMethod.GET)
- 	@ResponseBody
- 	public Resource getProjectsChangedPathCsvDownload(
+ 	public ResponseEntity<byte[]> getProjectsChangedPathCsvDownload(
  			@PathVariable("projectId") final int id) throws IOException {
- 		return commitService.getProjectChangedPathCsvByProjectId(id);
+ 		final byte[] bytes = commitService.getProjectChangedPathCsvByProjectId(id);
+ 		final HttpHeaders headers = new HttpHeaders();
+ 		headers.add("contet-type", MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE + ";Shift_JIS");
+ 		headers.set("Content-Disposition", String.format("filename=\"changedpaths_%s.csv\"", id));
+ 		return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
 	}
 }
