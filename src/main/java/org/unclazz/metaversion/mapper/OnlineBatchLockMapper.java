dc22eec182db44948dfe082121f94bf16c1b7d0c
mizuky fujitani <mizuky fujitani>
2016-04-26 13:19:52 +0900

    last_unlock_user_idのNOT NULL制約違反エラーが発生する問題への対策

--- src/main/java/org/unclazz/metaversion/mapper/OnlineBatchLockMapper.java
+++ src/main/java/org/unclazz/metaversion/mapper/OnlineBatchLockMapper.java
@@ -12,9 +12,10 @@ public interface OnlineBatchLockMapper {
 	int selectNextVal();
 	
 	@Insert("INSERT INTO online_batch_lock "
-			+ "(id, program_id, locked, last_lock_date, last_lock_user_id, last_unlock_date, system_boot_date) "
+			+ "(id, program_id, locked, last_lock_date, last_lock_user_id, "
+			+ "last_unlock_date, last_unlock_user_id, system_boot_date) "
 			+ "VALUES (#{lock.id}, #{lock.programId}, #{lock.locked}, #{lock.lastLockDate}, 0, "
-			+ "#{lock.lastUnlockDate}, #{lock.systemBootDate}) ")
+			+ "#{lock.lastUnlockDate}, 0, #{lock.systemBootDate}) ")
 	int insert(@Param("lock") OnlineBatchLock lock);
 	
 	@Select("SELECT id, program_id programId, locked, last_lock_date lastLockDate, "
